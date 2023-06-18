6ac1b57982c003dc05399df6f5196ba0c966dbd2
YaremaNatalia <yaremka.n@gmail.com>
2023-06-18 05:17:17 +0300

    add utils

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,31 +1,27 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
+import { generateRandomColor } from 'utils'; //імпортуємо функцію з папки utils (автоматично підтягує з index.js в папці, де знаходяться реекспорти всіх функцій та констант (CTRL пробіл щоб викликати підказку в фігурних дужках імпортування))
 // функція generateRandomColor() генерування випадкового кольору для елементів li
 
 export const Statistics = ({ title, stats }) => {
-  function generateRandomColor() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    const color = `rgb(${red}, ${green}, ${blue})`;
-    return color;
-  }
-
   return (
     <section className={css.statistics}>
       {/* titlе рендериться за умовою, якщо він є  */}
       {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
-        {stats.map(({ id, label, percentage }) => (
-          <li
-            key={id}
-            className={css.item}
-            style={{ backgroundColor: generateRandomColor() }}
-          >
-            <span className={css.label}>{label}</span>
-            <span className={css.percentage}>{percentage}%</span>
-          </li>
-        ))}
+        {stats.map(({ id, label, percentage }) => {
+          const randomColor = generateRandomColor();
+          return (
+            <li
+              key={id}
+              className={css.item}
+              style={{ backgroundColor: randomColor }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
